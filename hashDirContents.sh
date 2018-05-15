3c2cea97aca302ccf88bb91f2604d9bd3490d114
ez3r0sec <34905191+ez3r0sec@users.noreply.github.com>
2018-05-15 10:40:05 -0400

    Update hashDirContents.sh
    stylistic improvements

--- hashDirContents.sh
+++ hashDirContents.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+# hashDirContents.sh
 # hash all files in directory ($1)
+# Last Edited: 5/11/18 Julian Thies
 
+### VARIABLES
 userName="$(id -un)"
 hashDir="$1"
 dirContents="/tmp/dirContents.txt"
 hashStore="/home/$userName/Desktop/Hashes.txt"
 
+### FUNCTIONS
 function hash_file {
     if [ -d $line ] ; then
         echo "$line is a directory"
     else
         echo "$line" >> $hashStore
-        echo "md5    --> $(md5 $hashFile | awk '$1 {print $4}')" >> $hashStore
-        echo "sha1   --> $(shasum $hashFile | awk '$1 {print $1}')" >> $hashStore
-        echo "sha256 --> $(shasum -a 256 $hashFile | awk '$1 {print $1}')" >> $hashStore
+        echo "md5    --> $(md5 $hashFile | awk '{print $4}')" >> $hashStore
+        echo "sha1   --> $(shasum $hashFile | awk '{print $1}')" >> $hashStore
+        echo "sha256 --> $(shasum -a 256 $hashFile | awk '{print $1}')" >> $hashStore
         echo >> $hashStore
     fi
 }
