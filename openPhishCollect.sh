4ae704747900b0c98329911345bf9d194fb0635a
ez3r0sec <34905191+ez3r0sec@users.noreply.github.com>
2018-05-15 10:51:10 -0400

    Update openPhishCollect.sh
    stylistic improvements

--- openPhishCollect.sh
+++ openPhishCollect.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # openPhishCollect.sh
 # collect domains from https://openphish.com/feed.txt
-# Last Edited: 10/17/17
+# Last Edited: 5/11/17
 
+### VARIABLES
 userName="$(id -un)"
 
 URL="https://openphish.com/feed.txt"
 feedFile="/home/$userName/Desktop/phishFeed.txt"
 
+### SCRIPT
 curl -s -o "$feedFile" "$URL"
