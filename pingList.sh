5ecebd4e468f610d5c94382c077ab28916a89eff
ez3r0sec <34905191+ez3r0sec@users.noreply.github.com>
2018-05-15 10:53:07 -0400

    Update pingList.sh
    stylistic improvements

--- pingList.sh
+++ pingList.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# ping list
-# param $1 is the path to the file to read
+# pingList.sh
+# ping a list of IPs passed in as a text file (paramenter 1)
+#+with the format below
 
 # file format
-##############################
 #10.0.0.1
 #10.0.0.2
 #10.0.0.240
 #10.0.0.5
 #10.0.4.8
-##############################
 
 if [ "$1" == "" ] ; then
 	echo "Pass in the IP file path as param 1"
