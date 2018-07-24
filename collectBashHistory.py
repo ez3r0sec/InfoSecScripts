58bdfc99c54db4234826691afcee70a8f0e0380a
Chips <34905191+ez3r0sec@users.noreply.github.com>
2018-07-23 21:24:51 -0400

    Update collectBashHistory.py
    Fixed spacing issues

--- collectBashHistory.py
+++ collectBashHistory.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # collectBashHist.py
 # collect all bash_history on a system for analysis (Python 2.7)
-# Last Edited: 7/20/18
+# Last Edited: 7/23/18
 
 ### IMPORT
 import os
@@ -18,23 +18,23 @@ def mk_dir(dir):
 		pass
                 
 def find_hist(path, dest):
-    if os.path.exists(path):
-        for path, dir, files in os.walk(path):
-            for file in files:
-                if file.endswith('bash_history'):
-                    fp = os.path.join(path, file)
-                    fpString = list(fp)
-                    # take filepath and replace "/" with "_" to name the destination file
-                    for i in range(len(fpString))
-                        if fpString[i] == "/":
-                            fpString[i] = "_"
-                            name = "".join(fpString)
-                            destFile = os.path.join(dest, name)
-                            shutil.copy(fp, destFile)
-                 else:
-                    pass
-    else:
-        pass
+	if os.path.exists(path):
+		for path, dir, files in os.walk(path):
+			for file in files:
+				if file.endswith('bash_history'):
+					fp = os.path.join(path, file)
+					fpString = list(fp)
+					# take filepath and replace "/" with "_" to name the destination file
+					for i in range(len(fpString))
+						if fpString[i] == "/":
+							fpString[i] = "_"
+					name = "".join(fpString)
+					destFile = os.path.join(dest, name)
+					shutil.copy(fp, destFile)
+				else:
+					pass
+	else:
+		pass
                                                                                 
 ### SCRIPT
 mk_dir(destDir)
