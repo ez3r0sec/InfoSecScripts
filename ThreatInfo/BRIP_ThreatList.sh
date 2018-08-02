bf7495b101aab91b3fd7a74f194d88adb6e6ac82
Chips <34905191+ez3r0sec@users.noreply.github.com>
2018-08-02 12:40:45 -0400

    Update BRIP_ThreatList.sh

--- ThreatInfo/BRIP_ThreatList.sh
+++ ThreatInfo/BRIP_ThreatList.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
+# BRIP_ThreatList.sh
+# hxxp://www.neblink[.]net/blocklist/IP-Blocklist.txt
+# Last Edited: 8/2/18 Julian Thies
 
-### www.neblink.net/blocklist/IP-Blocklist.txt
+### VARIABLES
+userName="$(id -un)"
+URL="http://www.neblink.net/blocklist/IP-Blocklist.txt"
+tmpFile="/tmp/list.txt"
+listFile="/home/$userName/Desktop/DRIP_ThreatList.txt"
+
+### FUNCTIONS
+function dl_list {
+	curl -o "$tmpFile" "$URL"
+}
+
+function process_list {
+	grep -v "#" "$tmpFile" >> "$listFile"
+}
+
+function clean_up {
+	if [ -e "$tmpFile" ] ; then
+		rm "$tmpFile"
+	fi
+}
+
+### SCRIPT
+dl_list
+process_list
+clean_up
