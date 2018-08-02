63f943eda9d8d9c45946ea8927476abb92b037cb
Chips <34905191+ez3r0sec@users.noreply.github.com>
2018-08-02 12:36:02 -0400

    Update banlist_BDSA.sh

--- ThreatInfo/banlist_BDSA.sh
+++ ThreatInfo/banlist_BDSA.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
+# banlist_BDSA.sh
+# hxxps://www.binarydefense[.]com/banlist.txt
+# Last Edited: 8/2/18 Julian Thies
 
-### www.binarydefense.com/banlist.txt
+### VARIABLES
+userName="$(id -un)"
+URL="https://www.binarydefense.com/banlist.txt"
+tmpFile="/tmp/list.txt"
+listFile="/home/$userName/Desktop/binarydefense_banlist.txt"
+
+### FUNCTIONS
+function dl_list {
+	curl -o "$tmpFile" "$URL"
+}
+
+function process_list {
+	grep -v "#" "$tmpFile" >> "$listFile"
+}
+
+function clean_up {
+	if [ -e "$tmpFile" ] ; then
+		rm "$tmpFile"
+	fi
+	if [ -e "$listFile" ] ; then
+		rm "$listFile"
+	fi
+}
+
+### SCRIPT
+dl_list
+process_list
+clean_up
