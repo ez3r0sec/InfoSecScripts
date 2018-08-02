173a331c5ac815304e672141d4d740dc7f9a125f
Chips <34905191+ez3r0sec@users.noreply.github.com>
2018-08-02 12:42:21 -0400

    Update threatlist_mr.sh

--- ThreatInfo/threatlist_mr.sh
+++ ThreatInfo/threatlist_mr.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
+# threatlist_mr.sh
+# http://matthewroberts.io/api/threatlist/latest
+# Last Edited: 8/2/18 Julian Thies
 
-### matthewroberts.io/api/threatlist/latest
+### VARIABLES
+userName="$(id -un)"
+URL="https://www.binarydefense.com/banlist.txt"
+tmpFile="/tmp/list.txt"
+listFile="/home/$userName/Desktop/mattroberts_threatlist.txt"
+
+### FUNCTIONS
+function dl_list {
+	curl -o "$tmpFile" "$URL"
+}
+
+function process_list {
+	grep -v "#" "$tmpFile" >> "$listFile"
+}
+
+function clean_up {
+	if [ -e "$tmpFile" ] ; then
+		rm "$tmpFile"
+	fi
+}
+
+### SCRIPT
+dl_list
+process_list
+clean_up
