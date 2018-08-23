210e88caa992737f4a4832b773aa43f36196d8ad
Chips <34905191+ez3r0sec@users.noreply.github.com>
2018-08-23 07:26:12 -0400

    Update hashFile.py

--- Hashing/hashFile.py
+++ Hashing/hashFile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # hashFile.py
 # pass in the target file as param 1
-# Last Edited: 5/11/18 Julian Thies
+# Last Edited: 8/23/18 Julian Thies
 
 ### IMPORTS
 import os.path
@@ -10,27 +10,28 @@ import sys
 
 ### FUNCTIONS
 def hash_file(filename):
-     if os.path.exists(filename):
-          bufferSize = 65536
-          md5Hash = hashlib.md5()
-          sha1Hash = hashlib.sha1()
-          sha256Hash = hashlib.sha256()
-          with open(filename, 'rb') as f:
-               while True:
-                    data = f.read(bufferSize)
-                    if not data:
-                         break
-                    md5Hash.update(data)
-                    sha1Hash.update(data)
-                    sha256Hash.update(data)
+	if os.path.exists(filename):
+		bufferSize = 65536
+		md5Hash = hashlib.md5()
+		sha1Hash = hashlib.sha1()
+		sha256Hash = hashlib.sha256()
+		with open(filename, 'rb') as f:
+			while True:
+				data = f.read(bufferSize)
+				if not data:
+					break
+				md5Hash.update(data)
+				sha1Hash.update(data)
+				sha256Hash.update(data)
 
-          print
-	  print("md5:    {0}".format(md5Hash.hexdigest()))        # calculate and display the hashes
-          print("sha1:   {0}".format(sha1Hash.hexdigest()))
-          print("sha256: {0}".format(sha256Hash.hexdigest()))
+		print
+		# calculate and display the hashes
+		print("md5:    {0}".format(md5Hash.hexdigest()))
+		print("sha1:   {0}".format(sha1Hash.hexdigest()))
+		print("sha256: {0}".format(sha256Hash.hexdigest()))
 
-     else:
-          print(filename + " does not exist")
+	else:
+		print(filename + " does not exist")
 
 ### SCRIPT
 arg = sys.argv[1]
